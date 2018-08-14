9ea37f8ba00294485a86f113bff805f6fd0c5331
Francois-Julien Alcaraz <fj@waver.co>
2018-08-14 13:00:29 +0400

    Initial Commit

---
+++ HelloWorldWithPods/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorldWithPods
+//
+//  Created by François-Julien Alcaraz on 14/08/2018.
+//  Copyright © 2018 François-Julien Alcaraz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
